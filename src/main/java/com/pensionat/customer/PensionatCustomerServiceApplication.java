3b753b75c12deb6b048ae46a34de5e38c5efa9e0
Igor Gomes <www.igorgomes@hotmail.com>
2026-08-20 01:14:45 +0200

    chore: initialize Spring Boot customer service structure

---
+++ src/main/java/com/pensionat/customer/PensionatCustomerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.pensionat.customer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PensionatCustomerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PensionatCustomerServiceApplication.class, args);
+	}
+
+}
