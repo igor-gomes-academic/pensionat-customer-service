6611fe9aa03031bb1b212efb16ca80d3e626a27e
niklasdahlen <dahlstromniklas1@gmail.com>
2026-08-28 14:10:28 +0200

    feature/client

---
+++ src/main/java/com/pensionat/customer/client/BookingClient.java
@@ -0,0 +1,35 @@
+package com.pensionat.customer.client;
+
+package com.pensionat.client;
+
+import com.pensionat.exception.ServiceUnavailableException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClient;
+import org.springframework.web.client.RestClientException;
+
+@Component
+public class BookingClient {
+
+    private final RestClient restClient;
+
+    public BookingClient(@Value("${booking.service.url}") String baseUrl) {
+        this.restClient = RestClient.builder()
+                .baseUrl(baseUrl)
+                .build();
+    }
+
+    public boolean customerHasActiveBookings(Long customerId) {
+        try {
+            Boolean result = restClient.get()
+                    .uri("/api/bookings/customer/{id}/has-active", customerId)
+                    .retrieve()
+                    .body(Boolean.class);
+            return Boolean.TRUE.equals(result);
+        } catch (RestClientException e) {
+            throw new ServiceUnavailableException(
+                    "Could not reach booking service. Please try again later."
+            );
+        }
+    }
+}
